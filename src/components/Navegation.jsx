bde97892baaba240f066b7b0f22f8a3261980607
ChrisDev <Christianpeya@hotmail.com>
2024-05-15 23:10:53 -0600

    Nav Resposivo terminado

--- src/components/Navegation.jsx
+++ src/components/Navegation.jsx
@@ -3,9 +3,9 @@ import "../App.css";
 import { CiLight, CiLinkedin } from "react-icons/ci";
 import { MdOutlineDarkMode } from "react-icons/md";
 import { useState } from "react";
+import Mode from "./Mode";
 
-const Navegation = () => {
-  const [day, setDay] = useState(false);
+const Navegation = ({ theme, handleChangeTheme }) => {
   const handleClick = (index) => {
     if (index === 0) {
       window.scroll({
@@ -39,28 +39,31 @@ const Navegation = () => {
     }
   };
   return (
-    <nav className=" fixed w-full contenedor z-50 ">
-      <div className="w-[80%] mx-auto py-5 flex justify-between text-white border-b-2 px-5">
-        <h2 className="font-[Roboto] font-[700] text-xl">Christian Peña Yañez</h2>
+    <nav className="fixed w-full contenedor z-50 max-sm:bottom-2 ">
+      <div
+        className="w-[80%] mx-auto py-5 flex justify-between dark:text-white border-b-2 px-5
+      max-sm:w-full max-sm:justify-center
+      "
+      >
+        <h2 className="font-[Roboto] font-[700] text-xl max-sm:hidden">
+          Christian Peña Yañez
+        </h2>
         <div className="flex gap-8">
-          <ul className="flex gap-8 cursor-pointer font-[Roboto] font-[400]">
+          <ul className="flex gap-8 cursor-pointer font-[Roboto] font-[400] max-sm:text-[12px] max-sm:gap-4">
             <li onClick={() => handleClick(0)}>Home</li>
             <li onClick={() => handleClick(1)}>Sobre mi </li>
             <li onClick={() => handleClick(2)}>Proyectos</li>
             <li onClick={() => handleClick(3)}>Tecnologias</li>
             <li onClick={() => handleClick(4)}>Contacto</li>
           </ul>
-          <div className="flex gap-8">
-            {day ? (
-              <MdOutlineDarkMode className=" w-[30px] h-[30px] p-1 rounded-lg text-white"/>
-            ) : (
-              <CiLight className=" w-[30px] h-[30px] p-1 rounded-lg text-white" />
-            )}
-
-            <img src="https://pbs.twimg.com/profile_images/1496885672627556362/jkvPwUuT_400x400.jpg" alt="" 
-            className="rounded-full w-8 cursor-pointer"
-            />
+          <div className="flex gap-8 max-sm:hidden">
+            <Mode theme={theme} handleChangeTheme={handleChangeTheme}/>
           </div>
+          <img
+            src="https://pbs.twimg.com/profile_images/1496885672627556362/jkvPwUuT_400x400.jpg"
+            alt=""
+            className="rounded-full w-8 cursor-pointer max-sm:hidden"
+          />
         </div>
       </div>
     </nav>
