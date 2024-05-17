865793e324ef1fee82ccc76f106ea199f048f6b3
ChrisDev <Christianpeya@hotmail.com>
2024-05-17 01:09:27 -0600

    Corregi un but para el modo mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className=' text-center pt-10 pb-2 dark:text-white max-sm:py-8 w-[90%] mx-auto'>
+    <footer className=' text-center pt-10 pb-2 dark:text-white max-sm:py-8 w-[90%] mx-auto max-sm:mb-10'>
         <h4>Esta pagina esta creada con mucho {'<3'} por Christian Peña Yañez</h4>
     </footer>
   )
