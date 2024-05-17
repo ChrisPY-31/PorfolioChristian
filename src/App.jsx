668e9434dcff6b592a3a52138025321f66bf3cac
ChrisDev <Christianpeya@hotmail.com>
2024-05-17 01:07:02 -0600

    agregando errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
 import "./App.css";
 import AboutMe from "./components/AboutMe";
 import Contacto from "./components/Contacto";
+import Footer from "./components/Footer";
 import Inicio from "./components/Inicio";
 import Navegation from "./components/Navegation";
 import Proyectos from "./components/Proyectos";
 import Tecnologias from "./components/Tecnologias";
+import { useEffect, useState } from "react";
 function App() {
+
+  const [theme , setTheme] = useState(() =>{
+    if(window.matchMedia('(prefers-color-scheme:dark)').matches){
+      return "dark"
+    }
+    return "light"
+  })
+
+  console.log(theme)
+  const handleChangeTheme = () =>{
+    setTheme(prevTheme => prevTheme === "light" ? "dark" : "light")
+  }
+
+  useEffect(() =>{
+    if(theme === "dark"){
+      document.querySelector('html').classList.add('dark')
+    }
+    else{
+      document.querySelector('html').classList.remove('dark')
+    }
+  },[theme])
   return (
     <>
-      <Navegation />
-      <Inicio />
+      <Navegation theme={theme} handleChangeTheme={handleChangeTheme}/>
+      <Inicio theme={theme} handleChangeTheme={handleChangeTheme}/>
       <AboutMe />
       <Proyectos />
       <Tecnologias />
       <Contacto></Contacto>
+      <Footer></Footer>
     </>
   );
 }
