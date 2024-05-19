4bf6402d8c6777a2287a004827a1d2f6aeca7e72
ChrisDev <Christianpeya@hotmail.com>
2024-05-18 21:16:16 -0600

    Agregamos varias propiedades aun falta complementar mas

--- src/Proyectos.js
+++ src/Proyectos.js
@@ -1,9 +1,9 @@
 import tvMovies from './Images/TvMovies2.png'
-import granoDeOro from './Images/Grano-De-Oro'
-const proyectos = [
+import granoDeOro from './Images/Oro.jpeg'
+export const proyectos = [
     {
         title:'Grano de Oro',
-        subtitle:['Back-End', 'Front-End'],
+        subtitle:[' Back-End ', ' Front-End '],
         description:'E-commerce para la venta de cafe. este proyecto tuvo la finalidad de poner en practica todos nuestro conocimientos para enfrentar los desafios que podemos entrentar a futuro en una empresa simulando un ambiente laboral y colaboracion con un equipo de trabajo',
         image:`${granoDeOro}`,
         technologies:['JavaScript' , 'React' , 'Redux','Express','Sequelize','Node'],
@@ -13,9 +13,9 @@ const proyectos = [
         }
     },
     {
-        title:'TvPeliculas',
+        title:'Tv-Peliculas',
         subtitle:['Front-End'],
-        description:'',
+        description:'E-commerce para la venta de cafe. este proyecto tuvo la finalidad de poner en practica todos nuestro conocimientos para enfrentar los desafios que podemos entrentar a',
         image:`${tvMovies}`,
         technologies:['JavaScript', 'Tailwind' , 'React' , 'Redux', 'TypeScript'],
         deploid:{
@@ -25,10 +25,10 @@ const proyectos = [
     },
     {
         title:'Video Games',
-        subtitle:['Back-End','Front-End'],
-        description:'',
+        subtitle:[' Back-End ',' Front-End '],
+        description:'E-commerce para la venta de cafe. este proyecto tuvo la finalidad de poner en practica todos nuestro conocimientos para enfrentar los desafios que podemos entrentar a',
         technologies:['JavaScript' , 'React' , 'Redux','Express','Sequelize','Node'],
-        image:'',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOsuYDxvoDKhmnpc8EbIt1TybbDKvmVRtUr61EQi_CNA&s',
         deploid:{
             gitHud:'https://github.com/ChrisPY-31/Aplicacion_Videogames/tree/master/PI-Videogames-main',
             deploy:'',
